51d6dee4ccae54f7975322abe8956ef558f4cb9c
Houssnou <hosni.moubarak@gmail.com>
2019-11-27 09:45:39 -0500

    Skeleton

--- da.API/Controllers/ValuesController.cs
+++ da.API/Controllers/ValuesController.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using da.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace da.API.Controllers
 {
@@ -7,25 +11,34 @@ namespace da.API.Controllers
   [ApiController]
   public class ValuesController : ControllerBase
   {
+    private readonly DataContext _context;
+    public ValuesController(DataContext context)
+    {
+      _context = context;
+
+    }
+
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public async Task< IActionResult> GetValues()
     {
-      return new string[] { "value1", "value2" };
+      var values = await _context.Values.ToListAsync();
+      return Ok(values);
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public async Task<IActionResult> GetValue(int id)
     {
-      return "value";
+      var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id == id);
+      return Ok(value);
     }
 
     // POST api/values
     [HttpPost]
     public void Post([FromBody] string value)
     {
-      System.Console.WriteLine("You sent in value : "+value);
+      System.Console.WriteLine("You sent in value : " + value);
     }
 
     // PUT api/values/5
