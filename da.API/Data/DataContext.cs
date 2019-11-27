91ed1fdca25bcadc63526803bff1de7c19eb9e60
Houssnou <hosni.moubarak@gmail.com>
2019-11-26 22:41:29 -0500

    Project Init.

---
+++ da.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using da.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace da.API.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)  { }
+
+    public DbSet<Value> Values {get; set;}
+  }
+}
