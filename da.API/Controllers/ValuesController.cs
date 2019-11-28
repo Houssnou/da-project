11347a29252a19a896764e1ce05f61f713a46ed0
Houssnou <hosni.moubarak@gmail.com>
2019-11-28 13:03:47 -0500

    cleint side login and register

--- da.API/Controllers/ValuesController.cs
+++ da.API/Controllers/ValuesController.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using da.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace da.API.Controllers
 {
+  [Authorize]
   [Route("api/[controller]")]
   [ApiController]
   public class ValuesController : ControllerBase
@@ -19,18 +21,20 @@ namespace da.API.Controllers
     }
 
     // GET api/values
+    [AllowAnonymous]
     [HttpGet]
-    public async Task< IActionResult> GetValues()
+    public async Task<IActionResult> GetValues()
     {
       var values = await _context.Values.ToListAsync();
       return Ok(values);
     }
 
     // GET api/values/5
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public async Task<IActionResult> GetValue(int id)
     {
-      var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id == id);
+      var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
       return Ok(value);
     }
 
--- da.API/Startup.cs
+++ da.API/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using da.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace da.API
 {
@@ -29,11 +32,24 @@ namespace da.API
     {
       services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-      // cw
-      System.Console.WriteLine(Configuration.GetConnectionString("DefaultConnection"));
+      // System.Console.WriteLine(Configuration.GetConnectionString("DefaultConnection"));
 
       services.AddControllers();
       services.AddCors();
+      services.AddScoped<IAuthRepository, AuthRepository>();
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                  ValidateIssuerSigningKey = true,
+                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                                .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+
+                  ValidateIssuer = false,
+                  ValidateAudience = false
+                };
+              });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,8 +64,10 @@ namespace da.API
 
       app.UseRouting();
 
+      app.UseAuthentication();
+
       app.UseAuthorization();
-      
+
       app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
       app.UseEndpoints(endpoints =>
