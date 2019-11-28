298589b5c1b8aec2446c2f16d3ed76334b492c98
Houssnou <hosni.moubarak@gmail.com>
2019-11-28 14:55:46 -0500

    server side exception handling

--- da.API/Controllers/AuthController.cs
+++ da.API/Controllers/AuthController.cs
@@ -48,6 +48,7 @@ namespace da.API.Controllers
     [HttpPost("login")]
     public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
     {
+
       var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
       if (userFromRepo == null)
@@ -77,6 +78,7 @@ namespace da.API.Controllers
       {
         token = tokenHandler.WriteToken(token)
       });
+
     }
   }
 }
